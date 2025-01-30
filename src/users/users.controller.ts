817f3e3faaa33a17774984f47eb207390e80796d
Victor Ose Iyayi <veektaw@gmail.com>
2025-01-30 14:41:06 +0100

    Made basic crud and started mapping out the user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get() 
+    findAll(@Query('role') role?: | 'admin' | 'user' | 'guest') {
+        return {
+            name: 'John Doe',
+            email: 'XXXXXXXXXXXXXXXXXX',
+            age: 30,
+            role: role
+        }
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return {
+           id: id,
+        }
+    }
+
+    @Post()
+    create(@Body() user: {}) {
+        return user
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() user: {}) {
+        return {
+            id: id,
+            ...user
+        }
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return {
+            id: id,
+            deleted: true
+        }
+    }   
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            id: '1',
+            name: 'John Doe',
+            email: 'XXXXXXXXXXXXXXXXXX',
+            age: 30,
+            role: 'admin'
+
+        },
+        {
+            id: '2',
+            name: 'Jane Doe',
+            email: 'XXXXXXXXXXXXXXXXXX',
+            age: 30,
+            role: 'user'
+        },
+        {
+            id: '3',
+            name: 'Jack Doe',
+            email: 'XXXXXXXXXXXXXXXXXX',
+            age: 30,
+            role: 'guest'
+        }
+    ]
+}
