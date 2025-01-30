26e6bb6b5ed41d433aef083a9f49463aba84aae4
Victor Ose Iyayi <veektaw@gmail.com>
2025-01-30 15:05:44 +0100

    Added methods to the usersservice class

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 export class UsersService {
     private users = [
         {
-            id: '1',
+            id: 1,
             name: 'John Doe',
             email: 'XXXXXXXXXXXXXXXXXX',
             age: 30,
@@ -12,18 +12,59 @@ export class UsersService {
 
         },
         {
-            id: '2',
+            id: 2,
             name: 'Jane Doe',
             email: 'XXXXXXXXXXXXXXXXXX',
             age: 30,
             role: 'user'
         },
         {
-            id: '3',
+            id: 3,
             name: 'Jack Doe',
             email: 'XXXXXXXXXXXXXXXXXX',
             age: 30,
             role: 'guest'
         }
     ]
+
+    findAll(role?: 'admin' | 'user' | 'guest') {
+        if (role) {
+            return this.users.filter(user => user.role === role)
+        }
+        return this.users
+    }
+
+    findOne(id: number) {
+        const user = this.users.find(user => user.id === id)
+        return user
+    }
+
+    create(user: {name: string, email: string, age: number, role: 'admin' | 'user' | 'guest'}) {
+
+        const usersByhighestId = [...this.users].sort((a, b) => b.id - a.id)
+        const newUser = {
+            id: usersByhighestId[0].id + 1,
+            ...user
+        }
+        
+        this.users.push(newUser)
+        return newUser
+    }
+
+    update(id: number, updatedUser: {name?: string, email?: string, age?: number, role?: 'admin' | 'user' | 'guest'}) {
+        this.users = this.users.map(user => {
+            if (user.id === id) {
+                return {...user, ...updatedUser}
+            }
+            return user
+        })
+        return this.findOne(id)
+    }
+
+
+    delete(id: number) {
+        const removedUser = this.findOne(id)
+        this.users = this.users.filter(user => user.id !== id)
+        return removedUser
+    }
 }
